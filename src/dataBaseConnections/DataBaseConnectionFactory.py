1e29461bb8029a70ce96f2f6ca0a8fe8a44ed99e
Igor Canko Minotto <ra00066252@D-090875.pucsp.br>
2018-07-14 15:01:46 -0300

    Links

--- src/dataBaseConnections/DataBaseConnectionFactory.py
+++ src/dataBaseConnections/DataBaseConnectionFactory.py
@@ -7,6 +7,8 @@ class DataBaseConnectionFactory()
         raise InvalidDataBaseConfiguration("Engine not found or not implemented yet.")
 
     def createSQLiteConnectionFor(self, configuration):
+        # http://pythonclub.com.br/gerenciando-banco-dados-sqlite3-python-parte1.html
+        # https://stackoverflow.com/questions/372885/how-do-i-connect-to-a-mysql-database-in-python
         yield
 
 class InvalidDataBaseConfiguration(Exception):
